ca5f509a135d4877a06c651054462f6ea57f7cf7
Luis <luisbruggermartine@gmail.com>
2023-07-27 17:01:48 -0300

    Aula 67 - Thread

---
+++ src/luis/cursojava/aula67/Teste.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luis.cursojava.aula67;
+
+/**
+ *
+ * @author luisb
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        MinhaThread thread = new MinhaThread("Thread num.1", 600);
+        MinhaThread thread2 = new MinhaThread("Thread num.2", 900);
+
+//        thread.start();
+    }
+}
