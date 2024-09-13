65dfca9a933ef9e1a37d78fe57f1bf1ababb0bd1
Luis <luisbruggermartine@gmail.com>
2024-09-13 11:26:06 -0300

    Aula 54

---
+++ src/luis/cursojava/aula54/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package luis.cursojava.aula54;
+
+/**
+ *
+ * @author luisb
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        DiaSemana dia = DiaSemana.DOMINGO;
+
+        System.out.println(dia.toString() + " - " + dia.getValor());
+    }
+}
